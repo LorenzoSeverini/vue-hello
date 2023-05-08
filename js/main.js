33e632936917b5ff26de43d21a35b288a83b97d4
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-05-08 16:14:48 +0200

    main vue-js

--- js/main.js
+++ js/main.js
@@ -6,17 +6,67 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            // Data
-            // ************
+            // <!-- Descrizione:
+            // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data. -->
+            // <!-- Bonus:
+            // Aggiungere alla pagina un’immagine, presa anch’essa da un data. -->
+
+            title: 'Coffe House',
+
+            nameUser: '',
+            
+            ask: 'Do you want a coffee?',
+
+            menuList: [
+                {
+                    name: 'Cappuccino',
+                    price: 1.50,
+                    img: 'img/coffee1.jpeg'
+                },
+                {
+                    name: 'Espresso',
+                    price: 1.00,
+                    img: 'img/espresso.jpeg',
+                },
+                {
+                    name: 'Americano',
+                    price: 2.00,
+                    
+                },
+                {
+                    name: 'Latte',
+                    price: 2.50,
+                    
+                },
+            ]
 
 
         }
     },
-    methods: {
-        // Methods
-        // ************
-        // Method:
 
+    methods: {
+
+        menu () {
+            // show the menu
+            this.menuList.forEach((element) => {
+                console.log(element.name, element.price);
+                document.getElementById('menu').innerHTML += `
+                    <ul>
+                        <li>
+                            <a>${element.name} ${element.price}€</a>
+                            <img src="img/${element.img}.jpg" alt="${element.name}">
+                        </li>
+                    </ul>
+                `;
+            });
+            document.getElementById('yes').disabled = true;
+        },
+
+        reloadPage () {
+            // reload the page
+            location.reload();
+        },
 
     },
 }).mount('#app')
+
